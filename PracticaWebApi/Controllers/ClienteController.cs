9f3db4ab6ab836bd358f3bcfa403417d4d385cf8
User <User@DESKTOP-HC9U9JR>
2023-05-03 21:23:35 -0300

    Agregar archivos de proyecto.

---
+++ PracticaWebApi/Controllers/ClienteController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticaWebApi.Entidades;
+
+namespace PracticaWebApi.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+
+        [HttpGet]
+        public IEnumerable<Cliente> ListarClietnes()
+        {
+            return clientes;
+        }
+
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Id = 1;
+            cliente.Nombre = "Gerardo";
+            cliente.Apellido = "Vazquez";
+            cliente.Cuil = "132245";
+            cliente.TipoDocumento = "DU";
+            cliente.NroDocumento = 38869662;
+            cliente.EsEmpleadoBNA = true;
+            cliente.PaisOrigen = "ARGENTINA";
+
+            clientes.Add(cliente);
+
+            return clientes.FirstOrDefault(c => c.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult AgregarCliente([FromBody] Cliente cliente)
+        {
+            var clienteExistente = clientes.FirstOrDefault(c => c.Id == cliente.Id);
+            if (clienteExistente == null)
+            {
+                return NotFound();
+            }
+            clienteExistente.Nombre = cliente.Nombre;
+            clienteExistente.Apellido = cliente.Apellido;
+            clienteExistente.Cuil = cliente.Cuil;
+            clienteExistente.TipoDocumento = cliente.TipoDocumento;
+            clienteExistente.NroDocumento = cliente.NroDocumento;
+            clienteExistente.EsEmpleadoBNA = cliente.EsEmpleadoBNA;
+            clienteExistente.PaisOrigen = cliente.PaisOrigen;
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post(Cliente cliente)
+        {
+            clientes.Add(cliente);
+            return Ok();
+
+        }
+
+        //// GET: ClienteController/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: ClienteController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ClienteController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ClienteController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        //// GET: ClienteController/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ClienteController/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ PracticaWebApi/Entidades/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace PracticaWebApi.Entidades
+{
+    public class Cliente
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("apellido")]
+        public string Apellido { get; set; }
+
+        [JsonProperty("cuil")]
+        public string Cuil { get; set; }
+
+        [JsonProperty("tipoDocumento")]
+        public string TipoDocumento { get; set; }
+
+        [JsonProperty("nroDocumento")]
+        public int NroDocumento { get; set; }
+
+        [JsonProperty("esEmpleadoBNA")]
+        public bool EsEmpleadoBNA { get; set; }
+
+        [JsonProperty("paisOrigen")]
+        public string PaisOrigen { get; set; }
+    }
+}
